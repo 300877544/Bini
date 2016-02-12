58078b6d80651693b58f00db0767e64dc9b3ddca
BOBBY BABU <BOBBY BABU>
2016-02-11 22:14:15 -0500

    First commit

---
+++ SS_Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    private int health = 100;
+    public void damage(int value)
+    {
+        if(health >= value)
+        {
+            health -= value;
+
+        }
+        else
+        {
+            isGameOver = true;
+        }
+    }
+    public void fix(int value)
+    {
+        if(health < 100)
+        
+            health += value;
+        if(health > 100)
+        {
+            health = 100;
+        }
+
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isGameOver)
+        {
+            Debug.Log("Game Over");
+            //Destroy(player);
+            player.SetActive = false;
+        }
+	}
+}
